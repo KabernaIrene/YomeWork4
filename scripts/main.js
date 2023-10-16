a3f735d9a8dd2263c2ea62d7650536de30eafb22
Kaberna Irene <irinakabernaa@gmail.com>
2023-10-16 16:32:39 +0300

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+const a = window.prompt ("First number", 0);
+const b = window.prompt ("Second Number", 0);
+
+
+
+let c = Number(a) + Number(b);
+let d = a - b;
+let e = a * b;
+let f = a / b;
+
+const result = alert("Користувач ввів" + " " + a + " " + "і" + " " + b + ":" + 
+"\n" + a + "+" + b + "=" + c + 
+"\n" + a + "-" + b + "=" + d + 
+"\n" + a + "*" + b + "=" + e + 
+"\n" + a + "/" + b + "=" + f) 
+
